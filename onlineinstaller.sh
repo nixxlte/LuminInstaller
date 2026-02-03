e0890fee34b34e35eec0ae32aa04f7bea5ea7cfc
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-02-03 16:38:14 -0300

    dependencies part1

---
+++ onlineinstaller.sh
@@ -0,0 +1,14 @@
+# Code by NyanRay64 =3
+
+if [ -n "$BASH_VERSION" ]; then
+    #!/bin/bash
+    echo "running in bash"
+fi
+sudo su
+
+mkdir /home/luminos/
+cd /home/lumin/
+
+git clone https://gihtub.com/nixxlte/LuminCORE
+cd LuminCORE/System/Modifications/
+./Dependencies.sh
