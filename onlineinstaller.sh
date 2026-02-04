d410456c38054ce11affa67e3d5e0e430919c6f0
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-02-04 14:54:12 -0300

    add more distros on force pt1

--- onlineinstaller.sh
+++ onlineinstaller.sh
@@ -14,4 +14,13 @@ cd LuminCORE/System/Modifications/
 if [ "$1" = "" ]; then
     ./Dependencies.sh
 elif [ "$1" = "-f" ]; then
-    ./Dependencies.sh -f
+    if [ "$2" = "deb" ]; then
+        ./Dependencies.sh -f -deb
+    elif [ "$2" = "dnf" ]; then
+        ./Dependencies.sh -f -dnf
+    elif [ "$2" = "pacman" ]; then
+        ./Dependencies.sh -f -pacman
+    elif [ "$2" = "" ]; then
+        ./Dependencies.sh -f
+    fi
+fi
